0376a710e138cbefc01f6855a6a2d518a4b87304
Heissen Lopez <heissen.lopez@gmail.com>
2014-06-24 20:50:57 -0500

    Add dynamic page with argument

--- src/Controller/D8DemoController.php
+++ src/Controller/D8DemoController.php
@@ -21,4 +21,25 @@ class D8DemoController {
       '#markup' => t('My first page in drupal 8.'),
     );
   }
+
+  /**
+   * Callback d8Demo page example.
+   * @return markup
+   */
+  public function d8Demo2($name) {
+    return array(
+      '#prefix' => '<div class="d8-test-clas">',
+      '#suffix' => '</div>',
+      'hello' => array(
+        '#prefix' => '<em><h3 class="hello-name">',
+        '#suffix' => '</h3></em>',
+        '#markup' => t('Welcome, @name!', array('@name' => $name)),
+      ),
+      'message' => array(
+        '#prefix' => '<div class="hello-message">',
+        '#suffix' => '</div>',
+        '#markup' => t('This is a first dynamic page create for Drupal 8.'),
+      ),
+    );
+  }
 }
